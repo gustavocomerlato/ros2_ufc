d2e8b33fd8e7c1e6e4fcdc69bc700ce17b9ac213
nycolasRamires <nycolasramires2000@gmail.com>
2025-10-22 22:20:11 -0300

    Adicionando conteudo da aula 3

---
+++ Aula3/ex1_talker/src/listener.cpp
@@ -0,0 +1,43 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::placeholders;
+
+class SubscriberNode : public rclcpp::Node
+{
+private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr stringSubscriber_;
+
+    void the_funcion_to_subscribe(const std_msgs::msg::String::SharedPtr msg){
+        RCLCPP_INFO(this->get_logger(),"%s", msg->data.c_str());
+        RCLCPP_INFO(get_logger(), "the world");
+    }
+    
+public:
+SubscriberNode(const std::string& name) : Node(name) {
+
+    stringSubscriber_ = this->create_subscription<std_msgs::msg::String>("um_otimo_topico",10,
+                                                    std::bind(&SubscriberNode::the_funcion_to_subscribe, 
+                                                    this, _1)); //because the function has one arg
+    //RCLCPP_INFO(get_logger(), "Subscribber created");
+    }
+    ~SubscriberNode();
+};
+
+
+SubscriberNode::~SubscriberNode()
+{
+}
+
+
+int main(int argc, char **argv){
+    rclcpp::init(argc,argv);
+    
+    auto node = std::make_shared<SubscriberNode>("um_exemplar_ouvinte"); //'node' is pointer to node (smart pointer and shared)
+    
+    rclcpp::spin(node);
+    //exit
+    rclcpp::shutdown();
+    return 0;
+}
